bca07cea73e0ac6dc788d0fda41689ef720c4c26
ForbesLindesay <forbes@lindesay.co.uk>
2013-08-02 01:04:50 +0100

    Fix text wrapping

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var util = require('util')
 var insertCSS = require('insert-css')
 
-insertCSS('.object-explorer {background: white;padding: 1em;}.object-explorer .indent {padding-left: 1em;}' +
+insertCSS('.object-explorer {background: white;padding: 1em;white-space: nowrap;overflow: auto;}.object-explorer .indent {padding-left: 1em;}' +
           '.object-explorer .property-left {min-width: 10em;display: inline-block;}' + 
           '.object-explorer .expand-button {padding: 0.1em;margin: 0;font-size: 1em; height: auto;width: auto;background: none;border: none;color: black;}' +
           '.object-explorer .expand-button:hover {background: none;color: gray;}' +
